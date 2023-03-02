76dbe217233a69417fa9b7bc460c01800f1f9ad8
iamfromhe1l <96263909+iamfromhe1l@users.noreply.github.com>
2023-03-02 23:08:41 +0300

    Revert "Balance"

--- src/balance/dto/diff.balance.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsNumber } from 'class-validator';
-
-export class DiffBalanceDto {
-	@IsEmail()
-	email: string;
-
-	@IsNumber()
-	diff: number;
-}
--- src/balance/dto/edit.balance.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsNumber } from 'class-validator';
-
-export class EditBalanceDto {
-	@IsEmail()
-	email: string;
-
-	@IsNumber()
-	editedBalance: number;
-}
